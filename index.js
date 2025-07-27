80b53eded1e3b2af6a98aa174f2753810c4921cc
jaisri210 <jaisriarangasamy@gmail.com>
2025-07-27 17:54:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+let numofdrum=document.querySelectorAll(".drum").length;
+for(let i=0; i<numofdrum; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function()
+    {
+      let text=this.innerHTML;
+      switch(text){
+        case "w":
+            let tom1=new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            let tom2=new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s": 
+            let tom3=new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            let tom4=new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let crash=new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            let kick=new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            let snare=new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            console.log(text);          
+      }
+   }
+   );
+   }
+    
+
+
+
+
+
+
+
+/*console.log(this.style.color="yellow");*/
+/*var audio=new Audio("sounds/tom-1.mp3");
+    audio.play();*/
+
+
+
+
